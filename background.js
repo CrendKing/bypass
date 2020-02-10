12acc52fe1c4b79031cf98093893d3a264025f72
Crend King <975235+CrendKing@users.noreply.github.com>
2020-02-10 02:39:25 -0800

    Initial commit

---
+++ background.js
@@ -0,0 +1,26 @@
+function getLinkedUrl(url) {
+    const urlObj = new URL(url)
+    const params = urlObj.searchParams
+
+    for (const paramValue of params.values()) {
+        if (paramValue.startsWith('http://') || paramValue.startsWith('https://')) {
+            return paramValue
+        }
+    }
+
+    return null
+}
+
+function bypass() {
+    browser.tabs.query({ currentWindow: true, active: true }).then((tabs) => {
+        const tab = tabs[0]
+        const currentUrl = tab.url
+        const linkedUrl = getLinkedUrl(currentUrl)
+
+        if (linkedUrl != null) {
+            browser.tabs.update(tab.id, { url: linkedUrl });
+        }
+    })
+}
+
+browser.browserAction.onClicked.addListener(bypass)
